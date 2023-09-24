63d5c6fd349751c9abec2264c4f15772b9e779bc
Talha Qamar <84700933+Gamer-Kold@users.noreply.github.com>
2023-09-24 08:41:09 +0500

    Create kodin.js

---
+++ people/kodin.js
@@ -0,0 +1,11 @@
+{
+  "nick": "kodin",
+  "coordinates": [
+    33.534270,
+    73.052718
+  ],
+  "links": {
+    "Website": "https://kodin.cc",
+    "GitHub": "https://github.com/Gamer-Kold"
+  }
+}
